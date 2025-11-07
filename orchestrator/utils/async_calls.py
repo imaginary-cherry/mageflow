b7431204231d8f4207cbda7e98c7be6c63db4caa
YedidyaHKfir <Yedidya@dono.ai>
2025-11-07 13:21:22 +0200

    remove code

--- orchestrator/utils/async_calls.py
+++
@@ -1,8 +0,0 @@
-import inspect
-
-
-async def flexible_call(func, *args, **kwargs):
-    if inspect.iscoroutinefunction(func):
-        return await func(*args, **kwargs)
-    else:
-        return func(*args, **kwargs)
