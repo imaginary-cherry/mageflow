e30b3bca4b372fe2d23a0c2f541734bc33dfce6d
YedidyaHKfir <Yedidya@dono.ai>
2025-12-10 12:24:34 +0200

    we dont use these function anymore

--- mageflow/utils/pythonic.py
+++ mageflow/utils/pythonic.py
@@ -1,8 +1,6 @@
 import inspect
 from typing import Any
 
-from pydantic import BaseModel, create_model, ConfigDict
-
 ParamValidationType = dict[str, tuple[type, Any]]
 
 
@@ -14,18 +12,6 @@ def deep_merge(base: dict, updates: dict) -> dict:
         else:
             results[key] = value
     return results
-
-
-def extract_validators(data: dict) -> ParamValidationType:
-    return {key: (type(value), value) for key, value in data.items()}
-
-
-def create_model_from_validators(validators: ParamValidationType) -> type[BaseModel]:
-    return create_model(
-        "DynamicModel",
-        __config__=ConfigDict(arbitrary_types_allowed=True),
-        **validators,
-    )
 
 
 async def flexible_call(func, *args, **kwargs):
