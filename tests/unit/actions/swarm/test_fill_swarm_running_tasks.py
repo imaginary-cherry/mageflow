cad5c29772a7bc00575e6f47abe6b06c8a5c193b
YedidyaHKfir <yedidyakfir@gmail.com>
2026-01-22 17:04:11 +0200

    [33-make-swarm-tasks-idempotent] - move to conftest

--- tests/unit/actions/swarm/test_fill_swarm_running_tasks.py
+++ tests/unit/actions/swarm/test_fill_swarm_running_tasks.py
@@ -1,36 +1,9 @@
 import pytest
 import pytest_asyncio
 
-import mageflow
 from mageflow.swarm.messages import SwarmMessage
-from mageflow.swarm.model import SwarmTaskSignature, SwarmConfig
+from mageflow.swarm.model import SwarmTaskSignature
 from mageflow.swarm.workflows import fill_swarm_running_tasks
-from tests.integration.hatchet.models import ContextMessage
-
-
-@pytest_asyncio.fixture
-async def empty_swarm():
-    swarm_task = await mageflow.swarm(
-        task_name="test_swarm",
-        model_validators=ContextMessage,
-        config=SwarmConfig(max_concurrency=2),
-        is_swarm_closed=False,
-    )
-    yield swarm_task
-
-
-@pytest_asyncio.fixture
-async def swarm_task(empty_swarm):
-    swarm_task = empty_swarm
-    task = await mageflow.sign("item_task")
-    await swarm_task.add_task(task)
-    return swarm_task
-
-
-@pytest_asyncio.fixture
-async def swarm_with_ready_task(swarm_task: SwarmTaskSignature):
-    await swarm_task.tasks_left_to_run.aappend(swarm_task.tasks[0])
-    yield swarm_task
 
 
 @pytest.mark.asyncio
