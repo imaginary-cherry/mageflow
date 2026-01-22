ec4542f47f73281611bab1800049f7ed0eed4502
YedidyaHKfir <yedidyakfir@gmail.com>
2026-01-22 12:12:24 +0200

    [33-make-swarm-tasks-idempotent] - edit sanity tests for fill running swarm

--- tests/unit/swarm/test_fill_swarm_running_tasks.py
+++ tests/unit/swarm/test_fill_swarm_running_tasks.py
@@ -1,35 +1,47 @@
 import pytest
+import pytest_asyncio
 
-from mageflow.signature.model import TaskSignature
-from mageflow.signature.status import TaskStatus, SignatureStatus
+import mageflow
 from mageflow.swarm.messages import SwarmMessage
 from mageflow.swarm.model import SwarmTaskSignature, SwarmConfig
 from mageflow.swarm.workflows import fill_swarm_running_tasks
 from tests.integration.hatchet.models import ContextMessage
 
 
-@pytest.mark.asyncio
-async def test_handle_finish_tasks_sanity_starts_next_task(
-    mock_context, mock_batch_task_run, mock_activate_success, publish_state
-):
-    # Arrange
-    swarm_task = SwarmTaskSignature(
+@pytest_asyncio.fixture
+async def empty_swarm():
+    swarm_task = await mageflow.swarm(
         task_name="test_swarm",
         model_validators=ContextMessage,
         config=SwarmConfig(max_concurrency=2),
-        current_running_tasks=0,
         is_swarm_closed=False,
-        publishing_state_id=publish_state.key,
     )
-    await swarm_task.asave()
+    yield swarm_task
 
-    original_task = TaskSignature(task_name="test_task", model_validators=ContextMessage)
-    await original_task.asave()
 
-    batch_task = await swarm_task.add_task(original_task)
-    await swarm_task.tasks_left_to_run.aappend(batch_task.key)
+@pytest_asyncio.fixture
+async def swarm_task(empty_swarm):
+    swarm_task = empty_swarm
+    task = await mageflow.sign("item_task")
+    await swarm_task.add_task(task)
+    return swarm_task
 
-    msg = SwarmMessage(swarm_task_id=swarm_task.key)
+
+@pytest_asyncio.fixture
+async def swarm_with_ready_task(swarm_task: SwarmTaskSignature):
+    await swarm_task.tasks_left_to_run.aappend(swarm_task.tasks[0])
+    yield swarm_task
+
+
+@pytest.mark.asyncio
+async def test_handle_finish_tasks_sanity_starts_next_task(
+    swarm_with_ready_task: SwarmTaskSignature,
+    mock_context,
+    mock_batch_task_run,
+    mock_activate_success,
+):
+    # Arrange
+    msg = SwarmMessage(swarm_task_id=swarm_with_ready_task.key)
 
     # Act
     await fill_swarm_running_tasks(msg, mock_context)
@@ -37,93 +49,23 @@ async def test_handle_finish_tasks_sanity_starts_next_task(
     # Assert
     assert len(mock_batch_task_run.called_instances) == 1
 
-    reloaded_swarm = await SwarmTaskSignature.get_safe(swarm_task.key)
+    reloaded_swarm = await SwarmTaskSignature.get_safe(swarm_with_ready_task.key)
     assert len(reloaded_swarm.tasks_left_to_run) == 0
 
     mock_activate_success.assert_not_awaited()
 
 
 @pytest.mark.asyncio
-async def test_handle_finish_tasks_sanity_swarm_completes(
-    mock_context, mock_activate_success, publish_state
+async def test_handle_finish_tasks_no_tasks_left_edge_case(
+    empty_swarm: SwarmTaskSignature, mock_context
 ):
     # Arrange
-    swarm_task = SwarmTaskSignature(
-        task_name="test_swarm",
-        model_validators=ContextMessage,
-        config=SwarmConfig(max_concurrency=1),
-        current_running_tasks=0,
-        is_swarm_closed=True,
-        publishing_state_id=publish_state.key,
-        task_status=TaskStatus(status=SignatureStatus.DONE),
-    )
-    await swarm_task.asave()
-
-    task = TaskSignature(task_name="test_task", model_validators=ContextMessage)
-    await task.asave()
-
-    await swarm_task.tasks.aappend(task.key)
-    await swarm_task.finished_tasks.aappend(task.key)
-
-    msg = SwarmMessage(swarm_task_id=swarm_task.key)
-
-    # Act
-    await fill_swarm_running_tasks(msg, mock_context)
-
-    # Assert
-    mock_activate_success.assert_awaited_once_with(msg)
-
-
-@pytest.mark.asyncio
-async def test_handle_finish_tasks_no_tasks_left_edge_case(mock_context, publish_state):
-    # Arrange
-    swarm_task = SwarmTaskSignature(
-        task_name="test_swarm",
-        model_validators=ContextMessage,
-        config=SwarmConfig(max_concurrency=2),
-        current_running_tasks=0,
-        is_swarm_closed=False,
-        publishing_state_id=publish_state.key,
-    )
-    await swarm_task.asave()
-
-    msg = SwarmMessage(swarm_task_id=swarm_task.key)
+    msg = SwarmMessage(swarm_task_id=empty_swarm.key)
 
     # Act
     await fill_swarm_running_tasks(msg, mock_context)
 
     # Assert
     mock_context.log.assert_any_call(
-        f"Swarm item no new task to run in {swarm_task.key}"
+        f"Swarm item no new task to run in {empty_swarm.key}"
     )
-
-
-@pytest.mark.asyncio
-async def test_handle_finish_tasks_exception_during_activate_success_edge_case(
-    mock_context, mock_activate_success_error, publish_state
-):
-    # Arrange
-    swarm_task = SwarmTaskSignature(
-        task_name="test_swarm",
-        model_validators=ContextMessage,
-        config=SwarmConfig(max_concurrency=1),
-        current_running_tasks=0,
-        is_swarm_closed=True,
-        publishing_state_id=publish_state.key,
-        task_status=TaskStatus(status=SignatureStatus.DONE),
-    )
-    await swarm_task.asave()
-
-    task = TaskSignature(task_name="test_task", model_validators=ContextMessage)
-    await task.asave()
-
-    await swarm_task.tasks.aappend(task.key)
-    await swarm_task.finished_tasks.aappend(task.key)
-
-    msg = SwarmMessage(swarm_task_id=swarm_task.key)
-
-    # Act & Assert
-    with pytest.raises(RuntimeError):
-        await fill_swarm_running_tasks(msg, mock_context)
-
-    mock_activate_success_error.assert_awaited_once_with(msg)
