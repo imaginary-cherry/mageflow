adf72a4985523fa1df6120b6aca8d46d0c32c235
YedidyaHKfir <yedidyakfir@gmail.com>
2026-01-27 20:56:36 +0200

    [33-make-swarm-tasks-idempotent] - status also needs Meta as it is atomic class

--- mageflow/signature/status.py
+++ mageflow/signature/status.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from typing import ClassVar
 
 from rapyer import AtomicRedisModel
+from rapyer.config import RedisConfig
 
 
 class SignatureStatus(str, Enum):
@@ -22,6 +24,7 @@ class TaskStatus(AtomicRedisModel):
     status: SignatureStatus = SignatureStatus.PENDING
     last_status: SignatureStatus = SignatureStatus.PENDING
     worker_task_id: str = ""
+    Meta: ClassVar[RedisConfig] = RedisConfig(ttl=24 * 60 * 60, refresh_ttl=False)
 
     def is_canceled(self):
         return self.status in [SignatureStatus.CANCELED]
