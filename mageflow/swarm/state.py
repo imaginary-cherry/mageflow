d2a19e3acc850fe89510cb5c0b13d04e956c7823
YedidyaHKfir <yedidyakfir@gmail.com>
2026-01-27 16:39:00 +0200

    [33-make-swarm-tasks-idempotent] - publish state also has ttl

--- mageflow/swarm/state.py
+++ mageflow/swarm/state.py
@@ -1,9 +1,13 @@
-from pydantic import Field
-from rapyer import AtomicRedisModel
-from rapyer.types import RedisList
+from typing import ClassVar
 
 from mageflow.signature.types import TaskIdentifierType
+from pydantic import Field
+from rapyer import AtomicRedisModel
+from rapyer.config import RedisConfig
+from rapyer.types import RedisList
 
 
 class PublishState(AtomicRedisModel):
     task_ids: RedisList[TaskIdentifierType] = Field(default_factory=list)
+
+    Meta: ClassVar[RedisConfig] = RedisConfig(ttl=24 * 60 * 60, refresh_ttl=False)
