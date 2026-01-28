e25dc66d70159046f6cc83ba58fde3416e88ecde
YedidyaHKfir <yedidyakfir@gmail.com>
2026-01-28 18:38:11 +0200

    [33-make-swarm-tasks-idempotent] - idempotency test, check crash mid delete works

--- tests/unit/idempotency/test_chain_workflows_idempotent.py
+++ tests/unit/idempotency/test_chain_workflows_idempotent.py
@@ -73,6 +73,29 @@ async def test_chain_end_run_twice_callback_activated_once_idempotent(
 
 
 @pytest.mark.asyncio
+async def test__chain_end_fail_on_remove_task__able_to_delete(
+    redis_client,
+    mock_remove_task_with_delete,
+    mock_chain_activate_success,
+):
+    # Arrange
+    setup = await create_chain_test_setup(num_chain_tasks=3)
+
+    # Act
+    with pytest.raises(RuntimeError):
+        with patch.object(
+            ChainTaskSignature, "remove_task", side_effect=RuntimeError()
+        ):
+            await chain_end_task(setup.msg, setup.ctx)
+
+    sub_tasks_exists = await redis_client.exists(*setup.chain_signature.tasks)
+    assert not sub_tasks_exists
+    await chain_end_task(setup.msg, setup.ctx)
+    chain_exists = await redis_client.exists(*setup.chain_signature.key)
+    assert not chain_exists
+
+
+@pytest.mark.asyncio
 async def test_chain_error_crash_at_remove_current_task_retry_succeeds_idempotent(
     mock_remove_task_with_delete,
     mock_chain_activate_error,
@@ -109,3 +132,26 @@ async def test_chain_error_run_twice_callback_activated_once_idempotent(
 
     # Assert
     assert mock_chain_activate_error.call_count == 1
+
+
+@pytest.mark.asyncio
+async def test__chain_error_fail_on_remove_task__able_to_delete(
+    redis_client,
+    mock_remove_task_with_delete,
+    mock_chain_activate_error,
+):
+    # Arrange
+    setup = await create_chain_test_setup(num_chain_tasks=3)
+
+    # Act
+    with pytest.raises(RuntimeError):
+        with patch.object(
+            ChainTaskSignature, "remove_task", side_effect=RuntimeError()
+        ):
+            await chain_error_task(setup.msg, setup.ctx)
+
+    sub_tasks_exists = await redis_client.exists(*setup.chain_signature.tasks)
+    assert not sub_tasks_exists
+    await chain_end_task(setup.msg, setup.ctx)
+    chain_exists = await redis_client.exists(*setup.chain_signature.key)
+    assert not chain_exists
