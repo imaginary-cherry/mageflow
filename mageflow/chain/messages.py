29353ac3b6594a099f3e093a2e60f1063b7d9616
YedidyaHKfir <yedidyakfir@gmail.com>
2026-02-04 18:09:50 +0200

    [60-add-container_id-parameter-to-tasksignature-for-unified-container-reference] - fix workflows for chain, it is no longer a signature and get a normal message

--- mageflow/chain/messages.py
+++ mageflow/chain/messages.py
@@ -5,6 +5,16 @@ from pydantic import BaseModel
 from mageflow.models.message import ReturnValue
 
 
-class ChainCallbackMessage(BaseModel):
+class ChainMessage(BaseModel):
+    chain_task_id: str
+
+
+class ChainCallbackMessage(ChainMessage):
     chain_results: ReturnValue[Any]
     chain_task_id: str
+
+
+class ChainErrorMessage(ChainMessage):
+    error: str
+    original_msg: dict
+    error_task_key: str
