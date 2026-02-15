a5f94145377a4e0bffd2c6d221235d2121013713
YedidyaHKfir <yedidyakfir@gmail.com>
2026-02-15 16:09:00 +0200

    [split-to-pkgs] - added util to call task from signature

--- libs/third-magic/thirdmagic/client.py
+++ libs/third-magic/thirdmagic/client.py
@@ -1,9 +1,14 @@
 import abc
+import asyncio
 from abc import ABC
+from typing import TYPE_CHECKING
 
 from pydantic import BaseModel
 
 from thirdmagic.task import MageflowTaskDefinition
+
+if TYPE_CHECKING:
+    from thirdmagic.signatures.siganture import TaskSignature
 
 
 class BaseClientAdapter(ABC):
@@ -18,6 +23,25 @@ class BaseClientAdapter(ABC):
     @abc.abstractmethod
     async def acall_task_identifier(self, task_id: str, **kwargs):
         pass
+
+    @abc.abstractmethod
+    async def acall_signature(
+        self, signature: "TaskSignature", set_return_field: bool, **kwargs
+    ):
+        pass
+
+    async def acall_signatures(
+        self,
+        signatures: list["TaskSignature"],
+        set_return_field: bool,
+        msg_kwargs: list[dict],
+    ):
+        return asyncio.gather(
+            *[
+                self.acall_signature(signature, set_return_field, **kwargs)
+                for signature, kwargs in zip(signatures, msg_kwargs)
+            ]
+        )
 
     @abc.abstractmethod
     def should_task_retry(
@@ -36,7 +60,12 @@ class DefaultClientAdapter(BaseClientAdapter):
     async def acall_task_identifier(self, task_id: str, **kwargs):
         raise NotImplementedError("Set a client before we start")
 
+    async def acall_signature(
+        self, signature: "TaskSignature", set_return_field: bool, **kwargs
+    ):
+        raise NotImplementedError("Set a client before we start")
+
     def should_task_retry(
         self, task_definition: MageflowTaskDefinition, attempt_num: int, e: Exception
     ) -> bool:
-        pass
+        raise NotImplementedError("Set a client before we start")
