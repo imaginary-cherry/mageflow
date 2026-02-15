2f8e117ce01ea356f673365df1119aa38f2f291f
YedidyaHKfir <yedidyakfir@gmail.com>
2026-02-15 22:04:41 +0200

    [split-to-pkgs] - improve swarm creation func - improve hints and all unrecognized values go to kwargs

--- libs/third-magic/thirdmagic/swarm/creator.py
+++ libs/third-magic/thirdmagic/swarm/creator.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import overload, Any
 
 import rapyer
 
@@ -14,16 +15,35 @@ class SignatureOptions(TaskSignatureOptions):
     task_kwargs: dict
 
 
+@overload
+async def swarm(
+    tasks: list[TaskSignatureConvertible], **options: Unpack[TaskSignatureOptions]
+) -> SwarmTaskSignature: ...
+@overload
+async def swarm(
+    tasks: list[TaskSignatureConvertible], **options: Any
+) -> SwarmTaskSignature: ...
+
+
 async def swarm(
     tasks: list[TaskSignatureConvertible] = None,
     task_name: str = None,
-    **kwargs: Unpack[SignatureOptions],
+    **options: Unpack[SignatureOptions],
 ) -> SwarmTaskSignature:
     tasks = tasks or []
     task_name = task_name or f"swarm-task-{uuid.uuid4()}"
     publish_state = PublishState()
+    model_fields = list(SwarmTaskSignature.model_fields.keys())
+    kwargs = {
+        field_name: options.pop(field_name)
+        for field_name in model_fields
+        if field_name in options
+    }
     swarm_signature = SwarmTaskSignature(
-        **kwargs, task_name=task_name, publishing_state_id=publish_state.key
+        **kwargs,
+        task_name=task_name,
+        publishing_state_id=publish_state.key,
+        kwargs=options,
     )
     await rapyer.ainsert(publish_state, swarm_signature)
     await swarm_signature.add_tasks(tasks)
