5a4126f90970f46f3b47c981643cd1b3920f7ff0
YedidyaHKfir <yedidyakfir@gmail.com>
2026-02-16 16:28:16 +0200

    [split-to-pkgs] - send redis to ttl check

--- libs/mageflow/tests/unit/workflows/test_chain_end.py
+++ libs/mageflow/tests/unit/workflows/test_chain_end.py
@@ -7,7 +7,7 @@ from tests.unit.workflows.conftest import create_chain_test_setup
 
 
 @pytest.mark.asyncio
-async def test_chain_end_task_sanity(mock_workflow_run):
+async def test_chain_end_task_sanity(redis_client, mock_workflow_run):
     # Arrange
     setup = await create_chain_test_setup(
         num_chain_tasks=3,
@@ -22,7 +22,7 @@ async def test_chain_end_task_sanity(mock_workflow_run):
     triggered_signature_id = mock_workflow_run[0]._task_ctx[TASK_ID_PARAM_NAME]
     assert triggered_signature_id == setup.success_callback.key
 
-    await assert_task_has_short_ttl(setup.chain_signature.key)
+    await assert_task_has_short_ttl(redis_client, setup.chain_signature.key)
 
     for task in setup.chain_tasks:
-        await assert_task_has_short_ttl(task.key)
+        await assert_task_has_short_ttl(redis_client, task.key)
