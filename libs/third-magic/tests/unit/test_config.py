887201ddac6efd3322ffdbaaa8895282542c8ee3
YedidyaHKfir <yedidyakfir@gmail.com>
2026-02-16 21:16:25 +0200

    [split-to-pkgs] - move config tests to thirdmagic

--- libs/third-magic/tests/unit/test_config.py
+++ libs/third-magic/tests/unit/test_config.py
@@ -1,19 +1,19 @@
 import pytest
+
+import thirdmagic
+from tests.unit.messages import ContextMessage
 from thirdmagic.errors import TooManyTasksError
 from thirdmagic.swarm.model import SwarmConfig
-
-import mageflow
-from tests.integration.hatchet.models import ContextMessage
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(["max_task_allowed"], [[2], [1], [5]])
-async def test_add_task_exceeds_max_task_allowed_error(max_task_allowed):
+async def test_add_task_exceeds_max_task_allowed_error(mock_task_def, max_task_allowed):
     # Arrange
     initial_tasks = [
-        await mageflow.asign(f"test_task_{i}") for i in range(max_task_allowed)
+        await thirdmagic.sign(f"test_task_{i}") for i in range(max_task_allowed)
     ]
-    swarm_signature = await mageflow.aswarm(
+    swarm_signature = await thirdmagic.swarm(
         task_name="test_swarm",
         tasks=initial_tasks,
         model_validators=ContextMessage,
@@ -21,6 +21,6 @@ async def test_add_task_exceeds_max_task_allowed_error(max_task_allowed):
     )
 
     # Act & Assert
-    extra_task = await mageflow.asign("test_task_last")
+    extra_task = await thirdmagic.sign("test_task_last")
     with pytest.raises(TooManyTasksError):
         await swarm_signature.add_task(extra_task)
