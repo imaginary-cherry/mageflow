9bf5e2d2b695aaea8750dbe51d955b538a3d941e
YedidyaHKfir <yedidyakfir@gmail.com>
2026-04-06 13:08:39 +0300

    added error callback checks

--- libs/mageflow/tests/integration/hatchet/signature/test_workflow_signing.py
+++ libs/mageflow/tests/integration/hatchet/signature/test_workflow_signing.py
@@ -166,7 +166,13 @@ async def test_signed_dag_workflow_timeout(
         hatchet_client_init.hatchet,
     )
     message = WorkflowTestMessage(base_data=test_ctx, timeout_at_step=2)
-    signature = await mageflow.asign(test_dag_wf)
+    error_cb = await mageflow.asign(error_callback)
+    success_cb = await mageflow.asign(task1_callback)
+    signature = await mageflow.asign(
+        test_dag_wf,
+        error_callbacks=[error_cb],
+        success_callbacks=[success_cb],
+    )
 
     # Act
     await signature.aio_run_no_wait(message, options=trigger_options)
@@ -175,6 +181,8 @@ async def test_signed_dag_workflow_timeout(
     await asyncio.sleep(7)
     runs = await get_runs(hatchet, ctx_metadata)
     assert_signature_failed(runs, signature)
+    assert_signature_done(runs, error_cb)
+    assert_signature_not_called(runs, success_cb)
     await assert_redis_is_clean(redis_client)
 
 
@@ -193,7 +201,13 @@ async def test_signed_dag_workflow_retry_then_succeed(
     message = WorkflowTestMessage(
         base_data=test_ctx, retry_at_step=1, retry_succeed_on_attempt=2
     )
-    signature = await mageflow.asign(test_dag_wf)
+    success_cb = await mageflow.asign(task1_callback)
+    error_cb = await mageflow.asign(error_callback)
+    signature = await mageflow.asign(
+        test_dag_wf,
+        success_callbacks=[success_cb],
+        error_callbacks=[error_cb],
+    )
 
     # Act
     await signature.aio_run_no_wait(message, options=trigger_options)
@@ -202,6 +216,8 @@ async def test_signed_dag_workflow_retry_then_succeed(
     await asyncio.sleep(7)
     runs = await get_runs(hatchet, ctx_metadata)
     assert_signature_done(runs, signature, check_called_once=False)
+    assert_signature_done(runs, success_cb)
+    assert_signature_not_called(runs, error_cb)
     await assert_redis_is_clean(redis_client)
 
 
@@ -218,7 +234,13 @@ async def test_signed_dag_workflow_retry_to_failure(
         hatchet_client_init.hatchet,
     )
     message = WorkflowTestMessage(base_data=test_ctx, retry_at_step=2)
-    signature = await mageflow.asign(test_dag_wf)
+    error_cb = await mageflow.asign(error_callback)
+    success_cb = await mageflow.asign(task1_callback)
+    signature = await mageflow.asign(
+        test_dag_wf,
+        error_callbacks=[error_cb],
+        success_callbacks=[success_cb],
+    )
 
     # Act
     await signature.aio_run_no_wait(message, options=trigger_options)
@@ -227,4 +249,6 @@ async def test_signed_dag_workflow_retry_to_failure(
     await asyncio.sleep(13)
     runs = await get_runs(hatchet, ctx_metadata)
     assert_signature_failed(runs, signature)
+    assert_signature_done(runs, error_cb)
+    assert_signature_not_called(runs, success_cb)
     await assert_redis_is_clean(redis_client)
